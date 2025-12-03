3f1703f6d59ce437165bef57e9acd5f92e1fadd8
Raj-webdesigner <171815897+Raj-Shopify-Devloper@users.noreply.github.com>
2025-12-03 14:02:23 +0530

    Publish git

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/animation-site/',
   optimizeDeps: {
     include: ['react-slick > slick-carousel']  // Forces Vite to pre-bundle slick-carousel properly
   },
